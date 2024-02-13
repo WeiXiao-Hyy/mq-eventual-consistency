0fe66841c57659d60c33dc5f94429f463784493b
hujiale <hjlbupt@163.com>
2024-02-13 20:16:52 +0800

    first commit

---
+++ account-biz/account-api/src/test/java/com/alipay/accountapi/AccountApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alipay.accountapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
